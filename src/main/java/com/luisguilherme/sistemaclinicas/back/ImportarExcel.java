32b8af107bff1221cb738c92ae90ea1e167ed53a
Luis Guilherme Dias <59609978+Luiguie@users.noreply.github.com>
2024-04-12 11:09:56 -0300

    Add Importar Paciente, Consulta e Responsavel

--- src/main/java/com/luisguilherme/sistemaclinicas/back/ImportarExcel.java
+++ src/main/java/com/luisguilherme/sistemaclinicas/back/ImportarExcel.java
@@ -34,9 +34,11 @@ public class ImportarExcel {
             
             XSSFSheet enfermeiroSheet = book.getSheet("Enfermeiros");
             XSSFSheet medicosSheet = book.getSheet("Medicos");
+            XSSFSheet pacienteSheet = book.getSheet("Pacientes");
             
             importarEnfermeiro(enfermeiroSheet);
             importarMedico(medicosSheet);
+            importarPaciente(pacienteSheet);
             
             book.close();
         }
@@ -103,7 +105,7 @@ public class ImportarExcel {
     private void importarMedico(XSSFSheet sheet){
         // Pulando a primeira linha que tem o cabeçalho
         int startIndex = 1;
-
+        
         // itero e preencho o objeto
         for (int i = startIndex; i <= sheet.getLastRowNum(); i++){
             Row row = sheet.getRow(i);
@@ -156,6 +158,131 @@ public class ImportarExcel {
                     row.getCell(17).getStringCellValue());
                 
                 back.getMedicos().add(e);
+            }
+            
+            
+        }
+    }
+    
+   private ArrayList<Responsavel> loadResponsaveis(XSSFSheet sheetPai, long idPaciente){
+       int startIndex = 1;
+       
+       XSSFWorkbook book = sheetPai.getWorkbook();
+       XSSFSheet sheet = book.getSheet("Responsaveis");
+       ArrayList<Responsavel> responsaveis = new ArrayList<Responsavel>();
+       
+        // itero e preencho o objeto
+        for (int i = startIndex; i <= sheet.getLastRowNum(); i++){
+            Row row = sheet.getRow(i);
+            
+            if (row == null){
+                continue;
+            }
+            
+            if((long) row.getCell(5).getNumericCellValue() == idPaciente){
+                Responsavel r = new Responsavel(
+                        (long) row.getCell(0).getNumericCellValue(), 
+                        row.getCell(1).getStringCellValue(), 
+                        row.getCell(2).getStringCellValue(), 
+                        row.getCell(3).getStringCellValue(), 
+                        row.getCell(4).getStringCellValue());
+                
+                responsaveis.add(r);
+            }
+            
+        }
+        return responsaveis;
+   }
+   private ArrayList<ConsultaMedica> loadConsultas(XSSFSheet sheetPai, long idPaciente){
+       int startIndex = 1;
+       
+       XSSFWorkbook book = sheetPai.getWorkbook();
+       XSSFSheet sheet = book.getSheet("Consultas Medicas");
+       ArrayList<ConsultaMedica> consultas = new ArrayList<ConsultaMedica>();
+       
+        // itero e preencho o objeto
+        for (int i = startIndex; i <= sheet.getLastRowNum(); i++){
+            Row row = sheet.getRow(i);
+            
+            if (row == null){
+                continue;
+            }
+            
+            if((long) row.getCell(1).getNumericCellValue() == idPaciente){
+                ConsultaMedica c = new ConsultaMedica(
+                        (long) row.getCell(0).getNumericCellValue(),
+                        idPaciente, 
+                        (long) row.getCell(2).getNumericCellValue(),
+                        row.getCell(6).getStringCellValue(), 
+                        row.getCell(3).getStringCellValue(), 
+                        row.getCell(5).getStringCellValue(),
+                        row.getCell(4).getBooleanCellValue());
+                
+                consultas.add(c);
+            }
+            
+        }
+        return consultas;
+   }
+    private void importarPaciente(XSSFSheet sheet){
+        // Pulando a primeira linha que tem o cabeçalho
+        int startIndex = 1;
+        
+        // itero e preencho o objeto
+        for (int i = startIndex; i <= sheet.getLastRowNum(); i++){
+            Row row = sheet.getRow(i);
+            
+            if (row == null){
+                continue;
+            }
+            
+            Endereco end = new Endereco(
+                    row.getCell(3).getStringCellValue(), 
+                    (int) row.getCell(4).getNumericCellValue(), 
+                    row.getCell(5).getStringCellValue(), 
+                    row.getCell(6).getStringCellValue(), 
+                    row.getCell(7).getStringCellValue(), 
+                    (int) row.getCell(8).getNumericCellValue());
+            
+            ContatoTelEmail cont = new ContatoTelEmail(
+                    row.getCell(9).getStringCellValue(), 
+                    row.getCell(10).getStringCellValue(), 
+                    row.getCell(11).getStringCellValue());
+            
+            if(row.getCell(12).getStringCellValue() == "Masculino"){
+                Paciente e = new Paciente(
+                    (long) row.getCell(0).getNumericCellValue(),
+                    row.getCell(15).getStringCellValue(),
+                    row.getCell(1).getStringCellValue(),
+                    row.getCell(2).getDateCellValue(),
+                    end,
+                    cont,
+                    Genero.MASCULINO,
+                    loadResponsaveis(sheet,
+                            (long) row.getCell(0).getNumericCellValue()));
+                
+                e.setHistoricoConsultasMedicas(
+                        loadConsultas(sheet, 
+                        (long) row.getCell(0).getNumericCellValue()));
+                back.getPacientes().add(e);
+            }
+            else{
+                Paciente e = new Paciente(
+                    (long) row.getCell(0).getNumericCellValue(),
+                    row.getCell(15).getStringCellValue(),
+                    row.getCell(1).getStringCellValue(),
+                    row.getCell(2).getDateCellValue(),
+                    end,
+                    cont,
+                    Genero.FEMININO,
+                    loadResponsaveis(sheet,
+                            (long) row.getCell(0).getNumericCellValue()));
+                
+                e.setHistoricoConsultasMedicas(
+                        loadConsultas(sheet, 
+                        (long) row.getCell(0).getNumericCellValue()));
+                
+                back.getPacientes().add(e);
             }
             
             
